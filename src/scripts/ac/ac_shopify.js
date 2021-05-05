8851170d6d9cf25cc70a7ee1936f24134ed8c82e
Richard Arnold <richard@ambercouch.co.uk>
2021-05-05 11:37:30 +0100

    BBD - US - Added function to set country cookie on cancel

--- src/scripts/ac/ac_shopify.js
+++ src/scripts/ac/ac_shopify.js
@@ -3,7 +3,7 @@
  * Created by Richard on 19/09/2016.
  */
 
-console.log('ACSHOPIFY 31032021 filckty test');
+console.log('ACSHOPIFY 31032021 IPSTACK');
 const ACSHOPIFY = {
   common: {
     init: function() {
@@ -11,6 +11,8 @@ const ACSHOPIFY = {
       $(document).on('click', function(){
         // console.log('clicked the document');
       });
+
+
 
       // set IP - 'check' gets current users ip or set an ip address '123.123.123.123'
       const ip = 'check';
@@ -32,24 +34,41 @@ const ACSHOPIFY = {
             let visitorCountryCode = json.country_code;
 
             if (siteCountries.includes(visitorCountryCode)) {
-
               localVisitor = true;
+              Cookies.set('_bbd-country-confirm-us', 'true', { expires: 30 });
             } else {
-
               localVisitor = false;
               modalCountrySelect.open();
             }
           },
+          error: function(json) {
+            console.log('ipstack error');
+            console.log(json);
+            Cookies.set('_bbd-country-confirm-us', 'true', { expires: 30 });
+          },
         });
       }
 
-      $(document).on('confirmation', '.c-remodal-country-select', function () {
+      $(document).on('closed', '.c-remodal-country-select', function (e) {
         console.log('Confirmation country');
-        Cookies.set('_bbd-country-confirm-us', 'true', { expires: 30 });
+        if(e.reason == 'confirmation'){
+          console.log('closed Confirmation');
+          console.log('e.reason');
+          console.log(e.reason);
+
+          Cookies.set('_bbd-country-confirm-us', 'true', { expires: 30 });
+        }else{
+          console.log('closed cancelled');
+          console.log('e.reason');
+          console.log(e.reason);
+          Cookies.set('_bbd-country-confirm-us', 'true', { expires: 7 });
+        }
         confirmUs = Cookies.get('_bbd-country-confirm-us');
         console.log('confirmUs');
         console.log(confirmUs);
       });
+
+
 
       if (window.navigator.cookieEnabled) {
         document.documentElement.className = document.documentElement.className.replace(
